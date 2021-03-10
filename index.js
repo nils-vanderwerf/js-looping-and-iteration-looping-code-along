9de93f77a1f248a73f50854ed2214b54bcb89b43
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-11 00:31:19 +1100

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const people = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(people) {
+    let messages = [];
+    for (let i = 0; i < people.length; i++) {
+      console.log(`Thank you, ${people[i]}, for the wonderful surprise gift!`);  
+      messages[i] = `Thank you, ${people[i]}, for the wonderful surprise gift!`;
+    }
+
+    console.log(messages);
+    return messages;
+  }
+
+
+function countDown(number) {
+    for (let i = number; i >= 0 ; i--) {
+        console.log(i);  
+      }
+}
